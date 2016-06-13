6314733a66a1fe1e1ede42573aa541a5317c99f0
supermancbs <clark.sacktor@gmail.com>
2016-06-13 08:21:18 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  attr_accessor :title, :author 
+  
+  def initialize(title)
+    @title = title
+  end 
+
+  def author_name
+    if self.author==nil
+      return nil
+    else 
+      return self.author.name
+    end
+  end 
+
+
+end 
